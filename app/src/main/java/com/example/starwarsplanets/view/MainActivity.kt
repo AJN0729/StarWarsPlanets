147ef53b0e05ca73299e63a8444fcc716ae16c09
AJN0729 <andrewn0729@student.k12.tulsaschools.org>
2022-04-01 10:46:48 -0500

    added a lot of stuff, firebase broken.

--- app/src/main/java/com/example/starwarsplanets/view/MainActivity.kt
+++ app/src/main/java/com/example/starwarsplanets/view/MainActivity.kt
@@ -2,11 +2,48 @@ package com.example.starwarsplanets.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.starwarsplanets.R
+import com.example.starwarsplanets.model.PlanetData
+import com.example.starwarsplanets.viewmodel.PlanetsAdapter
+import com.google.firebase.database.*
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var mDatabase: DatabaseReference
+    private lateinit var planetList: ArrayList<PlanetData>
+    private lateinit var mAdapter: PlanetsAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        planetList = ArrayList()
+        mAdapter = PlanetsAdapter(this, planetList)
+        recyclerPlanets.layoutManager = LinearLayoutManager(this)
+        recyclerPlanets.setHasFixedSize(true)
+
+        getPlanetsData()
+    }
+
+    private fun getPlanetsData() {
+        mDatabase = FirebaseDatabase.getInstance().getReference("Planets")
+        mDatabase.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                if (snapshot.exists()) {
+                    for (planetSnapshot in snapshot.children) {
+                        val planet = planetSnapshot.getValue(PlanetData::class.java)
+                        planetList.add(planet!!)
+                    }
+                    recyclerPlanets.adapter = mAdapter
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+               Toast.makeText(this@MainActivity,error.message, Toast.LENGTH_SHORT).show()
+            }
+        })
     }
 }
