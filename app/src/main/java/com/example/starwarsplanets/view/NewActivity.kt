b9690d7f5f7d244b70ac511f0ff2459d74e1cf6c
AJN0729 <andrewn0729@student.k12.tulsaschools.org>
2022-04-04 08:58:57 -0500

    eh.

--- app/src/main/java/com/example/starwarsplanets/view/NewActivity.kt
+++ app/src/main/java/com/example/starwarsplanets/view/NewActivity.kt
@@ -3,10 +3,27 @@ package com.example.starwarsplanets.view
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.starwarsplanets.R
+import com.example.starwarsplanets.viewmodel.getProgressDrawable
+import com.example.starwarsplanets.viewmodel.loadImage
+import kotlinx.android.synthetic.main.activity_new.*
+import kotlinx.android.synthetic.main.planet_list.*
+import kotlinx.android.synthetic.main.planet_list.planetName
+import kotlinx.android.synthetic.main.planet_list.planetType
 
 class NewActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_new)
+
+        val planetIntent = intent
+        val planetNameNew = planetIntent.getStringExtra("planetName")
+        val planetTypeNew = planetIntent.getStringExtra("planetType")
+        val planetDescriptionNew = planetIntent.getStringExtra("planetDescription")
+        val planetImageNew = planetIntent.getStringExtra("planetImage")
+
+        planetName.text = planetNameNew
+        planetType.text = planetTypeNew
+        planetDescription.text = planetDescriptionNew
+        planetImageDetail.loadImage(planetImageNew, getProgressDrawable(this))
     }
 }
